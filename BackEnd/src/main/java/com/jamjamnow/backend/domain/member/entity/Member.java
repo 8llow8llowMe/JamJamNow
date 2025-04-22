ac1dab3aeb57b1918db93c74af728c1193149f14
신동근 <tlsehdrms124@naver.com>
2025-04-22 17:25:23 +0900

    [BE] feat: 회원 엔티티 추가

---
+++ BackEnd/src/main/java/com/jamjamnow/backend/domain/member/entity/Member.java
@@ -0,0 +1,49 @@
+package com.jamjamnow.backend.domain.member.entity;
+
+import com.jamjamnow.backend.global.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+    @Index(name = "idx_email", columnList = "email")
+})
+public class Member extends BaseEntity {
+
+    @Id
+    @Comment("회원 아이디")
+    @Column(columnDefinition = "BIGINT UNSIGNED")
+    private Long id;
+    
+    @Comment("이메일")
+    @Column(nullable = false)
+    private String email;
+
+    @Comment("비밀번호")
+    @Column(columnDefinition = "VARCHAR(80)")
+    private String password;
+
+    @Comment("이름")
+    @Column(columnDefinition = "VARCHAR(40)", nullable = false)
+    private String name;
+
+    @Comment("닉네임")
+    @Column(columnDefinition = "VARCHAR(60)", nullable = false)
+    private String nickname;
+
+    @Comment("프로필 이미지 URL")
+    private String profileImage;
+}
