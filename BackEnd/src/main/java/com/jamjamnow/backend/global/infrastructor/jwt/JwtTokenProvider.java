4d05cae4a109e48aa7ee91491a577d8866fb98c5
신동근 <tlsehdrms124@naver.com>
2025-04-22 17:25:07 +0900

    [BE] feat: JWT 토큰 관련 프로퍼티 설정을 가져오는 DTO 추가 및 JWT 토큰 생성기 추가

---
+++ BackEnd/src/main/java/com/jamjamnow/backend/global/infrastructor/jwt/JwtTokenProvider.java
@@ -0,0 +1,13 @@
+package com.jamjamnow.backend.global.infrastructor.jwt;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+
+}
