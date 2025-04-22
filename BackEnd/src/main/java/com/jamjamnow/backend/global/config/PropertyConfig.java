8c738d51909e4d2aecc5c4eb0954562bc47f62a1
신동근 <tlsehdrms124@naver.com>
2025-04-22 17:24:12 +0900

    [BE] feat: 프로퍼티 스캔 관련 설정 추가

---
+++ BackEnd/src/main/java/com/jamjamnow/backend/global/config/PropertyConfig.java
@@ -0,0 +1,10 @@
+package com.jamjamnow.backend.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan(basePackages = "com.jamjamnow")
+public class PropertyConfig {
+
+}
