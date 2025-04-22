a8433615a7ddd6d6909e55bb834a621b9967ff5d
신동근 <tlsehdrms124@naver.com>
2025-04-22 17:49:38 +0900

    [BE] feat: 로그인 기능 개발

--- BackEnd/src/main/java/com/jamjamnow/backend/domain/member/service/MemberService.java
+++ BackEnd/src/main/java/com/jamjamnow/backend/domain/member/service/MemberService.java
@@ -1,8 +1,12 @@
 package com.jamjamnow.backend.domain.member.service;
 
+import com.jamjamnow.backend.domain.member.dto.MemberLoginRequest;
+import com.jamjamnow.backend.domain.member.dto.MemberLoginResponse;
 import com.jamjamnow.backend.domain.member.dto.MemberSignupRequest;
 
 public interface MemberService {
 
     void signupMember(MemberSignupRequest signupRequest);
+
+    MemberLoginResponse loginMember(MemberLoginRequest loginRequest);
 }
