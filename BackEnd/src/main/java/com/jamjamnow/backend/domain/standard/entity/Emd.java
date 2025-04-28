e3dee238c6146732672cad8f55e2f2bd6036cfa6
신동근 <tlsehdrms124@naver.com>
2025-04-28 17:29:16 +0900

    [BE] feat: 읍면동 테이블 정의

---
+++ BackEnd/src/main/java/com/jamjamnow/backend/domain/standard/entity/Emd.java
@@ -0,0 +1,56 @@
+package com.jamjamnow.backend.domain.standard.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "uk_emd_cd",
+            columnNames = {"emdCd"}
+        )
+    },
+    indexes = {
+        @Index(name = "idx_emd_cd", columnList = "emdCd"),
+        @Index(name = "idx_ctpv_nm", columnList = "ctpvNm"),
+        @Index(name = "idx_sgg_nm", columnList = "sggNm")
+    }
+)
+public class Emd {
+
+    @Id
+    @Comment("읍면동 ID")
+    @Column(columnDefinition = "BIGINT UNSIGNED")
+    private Long id;
+
+    @Comment("읍면동 코드")
+    @Column(length = 10, nullable = false)
+    private String emdCd;
+
+    @Comment("시도명")
+    @Column(length = 50, nullable = false)
+    private String ctpvNm;
+
+    @Comment("시군구명")
+    @Column(length = 50, nullable = false)
+    private String sggNm;
+
+    @Comment("읍면동명")
+    @Column(length = 50, nullable = false)
+    private String emdNm;
+}
