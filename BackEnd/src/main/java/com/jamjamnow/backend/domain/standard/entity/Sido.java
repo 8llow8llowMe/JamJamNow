a5d9bfd475dbf0eb30cfe75d1917635b2d08648d
신동근 <tlsehdrms124@naver.com>
2025-04-28 17:29:02 +0900

    [BE] feat: 시도 테이블 정의

---
+++ BackEnd/src/main/java/com/jamjamnow/backend/domain/standard/entity/Sido.java
@@ -0,0 +1,49 @@
+package com.jamjamnow.backend.domain.standard.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "uk_ctpv_cd",
+            columnNames = {"ctpvCd"}
+        )
+    },
+    indexes = {
+        @Index(name = "idx_ctpv_cd", columnList = "ctpvCd")
+    }
+)
+public class Sido {
+
+    @Id
+    @Comment("시도 ID")
+    @Column(columnDefinition = "BIGINT UNSIGNED")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Comment("시도코드")
+    @Column(length = 2, nullable = false)
+    private String ctpvCd;
+
+    @Comment("시도명")
+    @Column(length = 50, nullable = false)
+    private String ctpvNm;
+}
