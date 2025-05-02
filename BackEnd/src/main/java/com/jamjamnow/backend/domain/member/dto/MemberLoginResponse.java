5b2b44b4dfb835ad52094a0390956f0db74601bf
신동근 <tlsehdrms124@naver.com>
2025-05-02 17:24:01 +0900

    [BE] refactor: 각 서비스로 나누는 작업으로 인한 루트프로젝트 내 해당 코드들 삭제처리

--- BackEnd/src/main/java/com/jamjamnow/backend/domain/member/dto/MemberLoginResponse.java
+++
@@ -1,5 +0,0 @@
-package com.jamjamnow.backend.domain.member.dto;
-
-public record MemberLoginResponse(MemberInfo memberInfo) {
-
-}
--- BackEnd/src/main/java/com/jamjamnow/backend/global/infrastructor/jwt/dto/JwtTokenInfo.java
+++
@@ -1,7 +0,0 @@
-package com.jamjamnow.backend.global.infrastructor.jwt.dto;
-
-public record JwtTokenInfo(
-    String accessToken
-) {
-
-}
