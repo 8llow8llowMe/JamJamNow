b0b01728e17c27d40abdb647fd5d8bfb42c51e34
신동근 <tlsehdrms124@naver.com>
2025-05-02 17:33:10 +0900

    [BE] feat: persistence 모듈에 JPA 관련 설정 및 스노우플레이크, BaseEntity 등 각종 모듈 설정

---
+++ BackEnd/Module/persistence-module/src/main/java/com/jamjamnow/persistencemodule/global/config/PersistenceModuleConfig.java
@@ -0,0 +1,10 @@
+package com.jamjamnow.persistencemodule.global.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.jamjamnow.persistencemodule")
+public class PersistenceModuleConfig {
+
+}
