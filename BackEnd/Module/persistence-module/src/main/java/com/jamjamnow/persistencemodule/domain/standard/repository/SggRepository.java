189679e56fc4ee1519acb150516e38638c8636b6
tlsehdrms124 <tlsehdrms124@naver.com>
2025-05-05 01:51:18 +0900

    [BE] feat: 시군구 Repository단 정의 및 메서드 정의 (시군구 코드와 시도의 시도명을 토대로 시군구 데이터 찾기)

---
+++ BackEnd/Module/persistence-module/src/main/java/com/jamjamnow/persistencemodule/domain/standard/repository/SggRepository.java
@@ -0,0 +1,10 @@
+package com.jamjamnow.persistencemodule.domain.standard.repository;
+
+import com.jamjamnow.persistencemodule.domain.standard.entity.Sgg;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SggRepository extends JpaRepository<Sgg, Long> {
+
+    Optional<Sgg> findBySggCdAndSido_CtpvNm(String sggCd, String ctpvNm);
+}
