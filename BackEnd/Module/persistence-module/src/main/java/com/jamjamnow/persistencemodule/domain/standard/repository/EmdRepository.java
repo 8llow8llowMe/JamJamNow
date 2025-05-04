c80ef5a2d03c8624062e5594ad25bd1e4820540d
tlsehdrms124 <tlsehdrms124@naver.com>
2025-05-05 02:41:09 +0900

    [BE] feat: 레포지토리단 해당 시군구코드로 읍면동 목록 조회 기능 추가

--- BackEnd/Module/persistence-module/src/main/java/com/jamjamnow/persistencemodule/domain/standard/repository/EmdRepository.java
+++ BackEnd/Module/persistence-module/src/main/java/com/jamjamnow/persistencemodule/domain/standard/repository/EmdRepository.java
@@ -1,8 +1,10 @@
 package com.jamjamnow.persistencemodule.domain.standard.repository;
 
 import com.jamjamnow.persistencemodule.domain.standard.entity.Emd;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmdRepository extends JpaRepository<Emd, Long> {
 
+    List<Emd> findBySgg_SggCd(String sggCd);
 }
