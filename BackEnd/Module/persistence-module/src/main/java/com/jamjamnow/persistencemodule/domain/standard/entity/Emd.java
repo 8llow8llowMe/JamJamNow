2618d9cbe4d0c93118ab5a864f55aea4b1d52732
tlsehdrms124 <tlsehdrms124@naver.com>
2025-05-05 00:15:35 +0900

    [BE] refactor: 읍면동 엔티티 수정

--- BackEnd/Module/persistence-module/src/main/java/com/jamjamnow/persistencemodule/domain/standard/entity/Emd.java
+++ BackEnd/Module/persistence-module/src/main/java/com/jamjamnow/persistencemodule/domain/standard/entity/Emd.java
@@ -2,7 +2,12 @@ package com.jamjamnow.persistencemodule.domain.standard.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
@@ -30,22 +35,20 @@ public class Emd {
     @Id
     @Comment("읍면동 ID")
     @Column(columnDefinition = "BIGINT UNSIGNED")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Comment("읍면동 코드")
     @Column(length = 10, nullable = false)
     private String emdCd;
 
-    @Comment("시도명")
-    @Column(length = 50, nullable = false)
-    private String ctpvNm;
-
-    @Comment("시군구명")
-    @Column(length = 50, nullable = false)
-    private String sggNm;
-
     @Comment("읍면동명")
     @Column(length = 50, nullable = false)
     private String emdNm;
+
+    @Comment("시군구 ID (외래키)")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sgg_id", nullable = false)
+    private Sgg sgg;
 }
 
