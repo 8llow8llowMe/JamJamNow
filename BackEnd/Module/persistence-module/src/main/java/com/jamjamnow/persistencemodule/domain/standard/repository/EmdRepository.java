98117eda8d011dd18ab92ffffbf0daf3a486b2ed
tlsehdrms124 <tlsehdrms124@naver.com>
2025-05-05 01:50:31 +0900

    [BE] feat: 읍면동 Reository단 정의

---
+++ BackEnd/Module/persistence-module/src/main/java/com/jamjamnow/persistencemodule/domain/standard/repository/EmdRepository.java
@@ -0,0 +1,8 @@
+package com.jamjamnow.persistencemodule.domain.standard.repository;
+
+import com.jamjamnow.persistencemodule.domain.standard.entity.Emd;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmdRepository extends JpaRepository<Emd, Long> {
+
+}
