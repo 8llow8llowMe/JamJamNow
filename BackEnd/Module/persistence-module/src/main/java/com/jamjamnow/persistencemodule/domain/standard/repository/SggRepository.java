449fdbcfe233c99578a6de2cd34ddec3f1e774e4
tlsehdrms124 <tlsehdrms124@naver.com>
2025-05-05 02:40:21 +0900

    [BE] feat: 레포지토리단 해당 시도코드로 시군구 목록 조회 기능 추가

--- BackEnd/Module/persistence-module/src/main/java/com/jamjamnow/persistencemodule/domain/standard/repository/SggRepository.java
+++ BackEnd/Module/persistence-module/src/main/java/com/jamjamnow/persistencemodule/domain/standard/repository/SggRepository.java
@@ -1,10 +1,13 @@
 package com.jamjamnow.persistencemodule.domain.standard.repository;
 
 import com.jamjamnow.persistencemodule.domain.standard.entity.Sgg;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SggRepository extends JpaRepository<Sgg, Long> {
 
     Optional<Sgg> findBySggCdAndSido_CtpvNm(String sggCd, String ctpvNm);
+
+    List<Sgg> findBySido_CtpvCd(String ctpvCd);
 }
