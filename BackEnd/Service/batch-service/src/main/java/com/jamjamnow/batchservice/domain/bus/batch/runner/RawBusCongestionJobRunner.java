0c4582453c5f334fc3f53738f4fc8614823f36b0
tlsehdrms124 <tlsehdrms124@naver.com>
2025-05-06 23:57:09 +0900

    [BE] refactor: CLI 배치 잡 통합으로 인한 해당 CLI 잡 러너들 주석 처리

--- BackEnd/Service/batch-service/src/main/java/com/jamjamnow/batchservice/domain/bus/batch/runner/RawBusCongestionJobRunner.java
+++ BackEnd/Service/batch-service/src/main/java/com/jamjamnow/batchservice/domain/bus/batch/runner/RawBusCongestionJobRunner.java
@@ -3,24 +3,22 @@
 //import lombok.RequiredArgsConstructor;
 //import lombok.extern.slf4j.Slf4j;
 //import org.springframework.batch.core.Job;
-//import org.springframework.batch.core.JobParameters;
 //import org.springframework.batch.core.JobParametersBuilder;
 //import org.springframework.batch.core.launch.JobLauncher;
 //import org.springframework.boot.CommandLineRunner;
 //
 //@Slf4j
 //@RequiredArgsConstructor
-//public class RawBusUsageJobRunner implements CommandLineRunner {
+//public class RawBusCongestionJobRunner implements CommandLineRunner {
 //
 //    private final JobLauncher jobLauncher;
-//    private final Job rawBusUsageJob;
+//    private final Job rawBusCongestionJob;
 //
 //    @Override
 //    public void run(String... args) throws Exception {
 //        String oprYmd = null;
 //        String sidoArg = null;
 //
-//        // 1. 인자 파싱
 //        for (String arg : args) {
 //            if (arg.startsWith("--oprYmd=")) {
 //                oprYmd = arg.substring("--oprYmd=".length());
@@ -29,24 +27,19 @@
 //            }
 //        }
 //
-//        // 2. 날짜는 필수
 //        if (oprYmd == null || oprYmd.isBlank()) {
-//            log.info("[CLI] --oprYmd 인자가 없으므로 배치 작업을 실행하지 않습니다.");
+//            log.info("[CLI] --oprYmd 인자가 없으므로 배치 작업 실행 안 함");
 //            return;
 //        }
 //
-//        log.info("[CLI] 수집 날짜: {}, 시도코드: {}", oprYmd, sidoArg);
-//
-//        // 3. JobParametersBuilder 구성
 //        JobParametersBuilder builder = new JobParametersBuilder()
 //            .addString("oprYmd", oprYmd)
-//            .addLong("timestamp", System.currentTimeMillis()); // 중복 방지용
+//            .addLong("timestamp", System.currentTimeMillis());
 //
 //        if (sidoArg != null && !sidoArg.isBlank()) {
-//            builder.addString("sido", sidoArg); // null일 경우 생략
+//            builder.addString("sido", sidoArg);
 //        }
 //
-//        JobParameters jobParameters = builder.toJobParameters();
-//        jobLauncher.run(rawBusUsageJob, jobParameters);
+//        jobLauncher.run(rawBusCongestionJob, builder.toJobParameters());
 //    }
 //}
