d66a7a2c4506a609cf22c19fde47b8c3d1617d5a
tlsehdrms124 <tlsehdrms124@naver.com>
2025-05-06 23:43:37 +0900

    [BE] feat: 잡 이름에 따른 CLI 배치 작업 관련 통합 기능 개발 및 리팩토링 작업

--- BackEnd/Service/batch-service/src/main/java/com/jamjamnow/batchservice/domain/standard/batch/job/EmdBatchJob.java
+++ BackEnd/Service/batch-service/src/main/java/com/jamjamnow/batchservice/domain/standard/batch/job/EmdBatchJob.java
@@ -1,15 +1,12 @@
 package com.jamjamnow.batchservice.domain.standard.batch.job;
 
-import com.jamjamnow.batchservice.domain.standard.batch.runner.EmdJobRunner;
 import com.jamjamnow.batchservice.domain.standard.batch.tasklet.EmdTasklet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -23,21 +20,21 @@ public class EmdBatchJob {
     private final EmdTasklet emdTasklet;
 
     @Bean
-    public Job emdJob() {
-        return new JobBuilder("emdJob", jobRepository)
-            .start(emdStep())
+    public Job insertEmdJob() {
+        return new JobBuilder("insertEmdJob", jobRepository)
+            .start(insertEmdStep())
             .build();
     }
 
     @Bean
-    public Step emdStep() {
-        return new StepBuilder("emdStep", jobRepository)
+    public Step insertEmdStep() {
+        return new StepBuilder("insertEmdStep", jobRepository)
             .tasklet(emdTasklet, transactionManager)
             .build();
     }
 
-    @Bean
-    public CommandLineRunner emdJobRunner(JobLauncher jobLauncher, Job emdJob) {
-        return new EmdJobRunner(jobLauncher, emdJob);
-    }
+//    @Bean
+//    public CommandLineRunner emdJobRunner(JobLauncher jobLauncher, Job emdJob) {
+//        return new EmdJobRunner(jobLauncher, emdJob);
+//    }
 }
