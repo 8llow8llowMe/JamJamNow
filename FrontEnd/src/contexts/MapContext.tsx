52d75176aecf71cd2381757517c047f3042e728c
choiseongho <chltjdgh3@naver.com>
2025-05-06 10:03:22 +0900

    [FE] fix: KakaoMap context 개별 파일로 분리

--- FrontEnd/src/contexts/MapContext.tsx
+++ FrontEnd/src/contexts/MapContext.tsx
@@ -1,34 +1,4 @@
-// src/contexts/MapContext.tsx
-import { createContext, useContext, useState, useEffect } from "react";
-import { loadKakaoMap } from "@src/libs/kakaoMapLoader";
+import { createContext } from "react";
+import { MapContextType } from "@src/types/map";
 
-type MapContextType = {
-  map: kakao.maps.Map | null;
-  setMap: (map: kakao.maps.Map) => void;
-  isLoaded: boolean;
-};
-
-const MapContext = createContext<MapContextType | undefined>(undefined);
-
-export const MapProvider = ({ children }: { children: React.ReactNode }) => {
-  const [map, setMap] = useState<kakao.maps.Map | null>(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    loadKakaoMap().then(() => {
-      setIsLoaded(true);
-    });
-  }, []);
-
-  return (
-    <MapContext.Provider value={{ map, setMap, isLoaded }}>
-      {children}
-    </MapContext.Provider>
-  );
-};
-
-export const useMap = () => {
-  const context = useContext(MapContext);
-  if (!context) throw new Error("useMap must be used within a MapProvider");
-  return context;
-};
+export const MapContext = createContext<MapContextType | undefined>(undefined);
