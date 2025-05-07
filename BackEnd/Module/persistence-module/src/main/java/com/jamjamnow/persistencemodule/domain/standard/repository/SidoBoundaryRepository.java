dd845d079a23a01c739fccd12fea0bb0d090d406
tlsehdrms124 <tlsehdrms124@naver.com>
2025-05-07 23:13:16 +0900

    [BE] feat: 시도 경계 레포지토리 정의

---
+++ BackEnd/Module/persistence-module/src/main/java/com/jamjamnow/persistencemodule/domain/standard/repository/SidoBoundaryRepository.java
@@ -0,0 +1,8 @@
+package com.jamjamnow.persistencemodule.domain.standard.repository;
+
+import com.jamjamnow.persistencemodule.domain.standard.entity.SidoBoundary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SidoBoundaryRepository extends JpaRepository<SidoBoundary, Long> {
+
+}
